fbc384f5771ff3bc6747fc0c564d9ccc2faa4191
Gabriel Viernes <viernesgabriel@comcast.net>
2023-08-10 15:23:46 -0500

    added functions to script.js

--- prework-study-guide/prework-study-guide/assets/script.js
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -1,26 +1,29 @@
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-}
-else if ( topic === 'CSS') {
-    console.log("Let's study CSS");
-}
-else if( topic ==='Git') {
-    console.log("Let's study Git");
-}
-else if (topic === 'Javascript') {
-    console.log("Let's study Javascript");
-}
-else {
-    console.log('Please try again');
-}
-var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
-for (var i = 0; i < shapes.length; i++){
-    console.log(shapes[i]);
-} 
-*/
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
-for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+    
 }
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    }
+    else if ( randomTopic === 'CSS') {
+        console.log("Let's study CSS");
+    }
+    else if( randomTopic ==='Git') {
+        console.log("Let's study Git");
+    }
+    else if (randomTopic === 'Javascript') {
+        console.log("Let's study Javascript");
+    }
+    else {
+        console.log('Please try again');
+    }
+}
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first");
+selectTopic();
